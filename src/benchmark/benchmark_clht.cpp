29e12c133ea375560eb704748b25a6712e1bc18e
Xilin Tang <xt253@ct-ristenpart-compute01.coecis.cornell.edu>
2024-09-17 21:50:59 -0400

    feat: support CLHT benchmarking

---
+++ src/benchmark/benchmark_clht.cpp
@@ -0,0 +1,29 @@
+#include "benchmark_clht.h"
+#include "unistd.h"
+
+namespace tinyptr {
+
+const BenchmarkObjectType BenchmarkCLHT::TYPE = BenchmarkObjectType::CLHT;
+
+BenchmarkCLHT::BenchmarkCLHT(int n) : BenchmarkObject64(TYPE) {
+    tab = clht_create(n / ENTRIES_PER_BUCKET);
+    clht_gc_thread_init(tab, gettid());
+}
+
+uint8_t BenchmarkCLHT::Insert(uint64_t key, uint64_t value) {
+    return clht_put(tab, key, value);
+}
+
+uint64_t BenchmarkCLHT::Query(uint64_t key, uint8_t ptr) {
+    return clht_get(tab->ht, key);
+}
+
+void BenchmarkCLHT::Update(uint64_t key, uint8_t ptr, uint64_t value) {
+    clht_put(tab, key, value);
+}
+
+void BenchmarkCLHT::Erase(uint64_t key, uint8_t ptr) {
+    clht_remove(tab, key);
+}
+
+}  // namespace tinyptr
