c41d3918b38bc1450141147878268db6c54819d9
Xilin Tang <tangxilin2001@qq.com>
2024-09-19 12:26:30 -0400

    feat: support benchmarkiceberg

---
+++ src/benchmark/benchmark_iceberg.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <emmintrin.h>
+#include <pthread.h>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <thread>
+#include "benchmark_object_64.h"
+#include "benchmark_object_type.h"
+#include "iceberg_table.h"
+
+namespace tinyptr {
+
+class BenchmarkIceberg : public BenchmarkObject64 {
+   public:
+    static const BenchmarkObjectType TYPE;
+
+   private:
+    uint64_t auto_iceberg_slot_num(uint64_t size);
+
+   public:
+    BenchmarkIceberg(int n);
+
+    ~BenchmarkIceberg() = default;
+
+    uint8_t Insert(uint64_t key, uint64_t value);
+    uint64_t Query(uint64_t key, uint8_t ptr);
+    void Update(uint64_t key, uint8_t ptr, uint64_t value);
+    void Erase(uint64_t key, uint8_t ptr);
+
+   private:
+    pid_t tid;
+    iceberg_table tab;
+};
+
+}  // namespace tinyptr
