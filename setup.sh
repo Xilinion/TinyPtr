0b507227f2eaa099c3a3537dbaa1c879dc73dde6
BlackJackTone <tangxilin2001@qq.com>
2025-07-23 01:20:34 +0000

    including dependencies

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt-get install cmake
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+sudo apt-get install python3-dev
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-wheel
+sudo apt-get install python3-numpy
+sudo apt-get install python3-scipy
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-pandas
+sudo apt install -y libssl-dev
+sudo apt install -y numactl
