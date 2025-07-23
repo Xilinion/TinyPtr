b92cd1f4bbe9d4dc1e5264b825034701ff352a20
BlackJackTone <tangxilin2001@qq.com>
2025-07-23 21:19:25 +0000

    make blastht support ycsb

--- src/benchmark/benchmark_blast_ht.cpp
+++ src/benchmark/benchmark_blast_ht.cpp
@@ -33,6 +33,54 @@ void BenchmarkBlastHT::Erase(uint64_t key, uint8_t ptr) {
     tab->Free(key);
 }
 
+void BenchmarkBlastHT::YCSBFill(std::vector<uint64_t>& keys, int num_threads) {
+    std::vector<std::thread> threads;
+    size_t chunk_size = keys.size() / num_threads;
+
+    for (int i = 0; i < num_threads; ++i) {
+        size_t start_index = i * chunk_size;
+        size_t end_index =
+            (i == num_threads - 1) ? keys.size() : start_index + chunk_size;
+
+        threads.emplace_back([this, &keys, start_index, end_index]() {
+            for (size_t j = start_index; j < end_index; ++j) {
+                tab->Insert(keys[j], 0);
+            }
+        });
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+}
+
+void BenchmarkBlastHT::YCSBRun(std::vector<std::pair<uint64_t, uint64_t>>& ops,
+                               int num_threads) {
+    std::vector<std::thread> threads;
+    size_t chunk_size = ops.size() / num_threads;
+
+    for (int i = 0; i < num_threads; ++i) {
+        size_t start_index = i * chunk_size;
+        size_t end_index =
+            (i == num_threads - 1) ? ops.size() : start_index + chunk_size;
+
+        threads.emplace_back([this, &ops, start_index, end_index]() {
+            uint64_t value;
+            for (size_t j = start_index; j < end_index; ++j) {
+                if (ops[j].first == 1) {
+                    tab->Insert(ops[j].second, 0);
+                } else {
+                    tab->Query(ops[j].second, &value);
+                }
+            }
+        });
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+}
+
 void BenchmarkBlastHT::ConcurrentRun(
     std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& ops,
     int num_threads) {
--- src/benchmark/benchmark_blast_ht.h
+++ src/benchmark/benchmark_blast_ht.h
@@ -22,6 +22,10 @@ class BenchmarkBlastHT : public BenchmarkObject64 {
     void Update(uint64_t key, uint8_t ptr, uint64_t value);
     void Erase(uint64_t key, uint8_t ptr);
 
+    void YCSBFill(std::vector<uint64_t>& keys, int num_threads);
+    void YCSBRun(std::vector<std::pair<uint64_t, uint64_t>>& ops,
+                 int num_threads);
+
     void ConcurrentRun(
         std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& ops,
         int num_threads);
