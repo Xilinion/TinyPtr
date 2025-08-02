93a7fd3b17eac1fdcd4a6e4469f525ba1d0cf454
BlackJackTone <tangxilin2001@qq.com>
2025-08-02 20:52:40 +0000

    rerun for average

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -395,22 +395,10 @@ for case_id in 9 10; do
     done
 done
 
-# load factor support
-
-for case_id in 0; do
-    for object_id in 4; do
-        entry_id=0
-        for table_size in 67108864; do
-            for bin_size in 3 7 15 31 63 127; do
-                opt_num=$table_size
-                RunWithRetry "Run"
-                let "entry_id++"
-            done
-        done
-    done
-done
-
 # Throughput / Space Efficiency
+
+# Number of repetitions for each configuration
+num_rep=10
 
 for case_id in 1; do
     for object_id in "${no_resize_object_ids[@]}"; do
@@ -418,8 +406,29 @@ for case_id in 1; do
         for table_size in 16777215; do
             for load_factor in 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95; do
                 opt_num=$(printf "%.0f" $(echo "$table_size * $load_factor" | bc -l))
+                
+                # Run memory free only once per configuration
                 output=$(RunRandMemFree)
                 echo "$output"
+                
+                # Repeat the actual run 10 times
+                for ((rep=1; rep<=num_rep; rep++)); do
+                    RunWithRetry "Run"
+                    let "entry_id++"
+                done
+            done
+        done
+    done
+done
+
+# load factor support
+
+for case_id in 0; do
+    for object_id in 4; do
+        entry_id=0
+        for table_size in 16777215; do
+            for bin_size in 3 7 15 31 63 127; do
+                opt_num=$table_size
                 RunWithRetry "Run"
                 let "entry_id++"
             done
@@ -434,10 +443,10 @@ exit
 for case_id in 16; do
     for object_id in 4; do
         entry_id=0
-        for table_size in 67108864; do
+        for table_size in 16777215; do
             opt_num=$table_size
             for bin_size in 3 7 15 31 63 127; do
-                for load_factor in 0.99 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.91 0.90 0.89 0.88 0.87 0.86 0.85 0.84 0.83 0.82 0.81 0.80 0.79 0.78 0.77 0.76 0.75 0.74 0.73 0.72 0.71 0.70 0.69 0.68 0.67 0.66 0.65 0.64 0.63 0.62 0.61 0.60 0.59 0.58 0.57 0.56 0.55 0.54 0.53 0.52 0.51 0.50 0.49 0.48 0.47 0.46 0.; do
+                for load_factor in 0.99 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.91 0.90 0.89 0.88 0.87 0.86 0.85 0.84 0.83 0.82 0.81 0.80 0.79 0.78 0.77 0.76 0.75 0.74 0.73 0.72 0.71 0.70 0.69 0.68 0.67 0.66 0.65 0.64 0.63 0.62 0.61 0.60 0.59 0.58 0.57 0.56 0.55 0.54 0.53 0.52 0.51 0.50 0.49 0.48 0.47 0.46 0.45 0.44 0.43 0.42 0.41 0.40 0.39 0.38 0.37 0.36 0.35 0.34 0.33 0.32 0.31 0.30 0.29 0.28 0.27 0.26 0.25 0.24 0.23 0.22 0.21 0.20 0.19 0.18 0.17 0.16 0.15 0.14 0.13 0.12 0.11 0.10 0.09 0.08 0.07 0.06; do
                     output=$(Run)
                     echo "$output"
 
