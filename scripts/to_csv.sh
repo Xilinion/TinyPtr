a2406208bfb524b84fd3a92542207c6de0f13ca2
Xilin Tang <35692223+BlackJackTone@users.noreply.github.com>
2025-08-02 21:57:41 -0400

    Delete scripts directory

--- scripts/to_csv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python3 ./ycsb_to_csv.py
-python3 ./micro_to_csv.py
-python3 ./scaling_to_csv.py
-python3 ./progressive_latency_to_csv.py
-python3 ./load_factor_support_to_csv.py
-python3 ./throughput_space_eff_to_csv.py
-python3 ./tagging_with_type.py
